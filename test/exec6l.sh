e8d5c4fff6e4a8056faaf3e69840e8d98df7a0a0
Lacey Rainbolt <jlrainbolt@u.northwestern.edu>
2019-08-28 17:19:31 -0500

    Added 6l selection, updated prefiring

---
+++ test/exec6l.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+suffix="$1"
+id="$2"
+
+
+echo "Date:     " `date`
+echo "Host:     " $HOSTNAME
+echo "Location: " $TEMP
+echo ""
+
+echo "Loading ROOT..."
+source /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-slc6-gcc62-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.16.00-f8770/x86_64-slc6-gcc62-opt/bin/thisroot.sh
+echo "ROOTSYS = " $ROOTSYS
+
+tar -xzvf source.tar.gz
+cd "test"
+
+root.exe -q -b "../macros/Selection6l.cc(\"$suffix\", \"$id\")"
+
+cp *_${suffix}_*.root ${_CONDOR_SCRATCH_DIR}
+
+echo ""
+echo ""
+echo "Date:     " `date`
